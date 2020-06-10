2dd668fbf22b1d233d3f359ce2ddf5940c871ff1
RetroHipster <ethanrwillinger@gmail.com>
2020-06-09 23:47:28 -0500

    Implimented basic program structure - without tests.

---
+++ OutlierDetector.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <cstdlib>
+
+
+int main() {
+
+
+
+
+
+
+	return 0;
+}
+
+
+int FindOutlier(std::vector<int> arr) {
+	std::vector<int> odd;		//Odd and even array containers
+	std::vector<int> even;
+
+	//Determine if an int is even or odd and place it in the right vector, do this for each int in arr
+	for (int x : arr) {
+		
+
+	}
+
+
+}
+
